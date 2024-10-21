88bab5ee6f298c825a519c023d327cfe5423dc14
Symers <u47as23@abdn.ac.uk>
2024-10-22 00:51:55 +0100

    Cleaning of map, attempt to lock user in place

--- src/components/ChapelComponent.js
+++ src/components/ChapelComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 // Define container size and style
 const containerStyle = {
@@ -13,18 +13,43 @@ const center = {
   lng: -2.1017,
 };
 
+
+//Coordinates to isolate the Chapel
+const bounds = {
+  north: 57.164250, //North latitude boundary
+  south: 57.163938, //South latitude boundary
+  east: -2.100914,  //East longitude boundary
+  west:  -2.101950,  //West longitude boundary
+};
+
+//Options for the map
+const options = {
+  restriction: {
+    latLngBounds: bounds, //Restrict the map to the boundary
+    strictBounds: true, //Set true to restrict the boundary
+  },
+};
+
+//Test marker
+const testMarkerPosition = {
+  lat: 57.164133,  
+  lng: -2.101531,  
+};
+
 function MapComponent() {
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={15}
-        options={{
-            mapId: process.env.REACT_APP_MAP_ID // Add your Map ID here
-          }}
+        zoom={12}
+        options={options}
       >
         { /* You can add markers or other components here */ }
+        <Marker
+          position={testMarkerPosition} //Position of the marker
+          title="Test Marker"          //Tooltip title when hovering over the marker
+        />
       </GoogleMap>
     </LoadScript>
   );
