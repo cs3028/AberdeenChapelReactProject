7ba150b6e2f9da5b580f40d58bcc792df0cf403c
Sid <110429895+sidthesloth0@users.noreply.github.com>
2025-02-19 12:37:39 +0000

    yh

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,7 +4,12 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }) => {
     const token = localStorage.getItem("token"); // get token from local storage
 
-    return token ? children : <Navigate to="/login" />; // redirect if not logged in
+    // redirect if not logged in
+    if (token) {
+        return children;  // if user is logged in, render the dashboard
+    } else {
+        return <Navigate to="/login" />;  // if not logged in, redirect them to the login page
+    }
 };
 
 export default ProtectedRoute;
