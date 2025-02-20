c281b8de9e292251731ffcf29b976c5d44b46e10
Rorycameron14 <114499137+Rorycameron14@users.noreply.github.com>
2025-02-20 19:34:03 +0000

    pages added

---
+++ src/components/WarMemorial.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar'; 
+import Footer from './Footer'; 
+
+
+function WarMemorial() {
+  return (
+    <div className="WarMemorial">
+      <Navbar />
+      <h1>War Memorial</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default WarMemorial;
