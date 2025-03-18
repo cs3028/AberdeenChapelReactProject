022af88ec8d7b08a58afe0aaface5f4e2ad51395
Symers <u47as23@abdn.ac.uk>
2025-03-18 12:06:57 +0000

    Added All Info Pages (empty)

--- src/components/RoodScreen.jsx
+++ src/components/RoodScreen.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Footer from './Footer';
+import { useNavigate } from "react-router-dom";
+import "./moreInfo.css";
 
 function RoodScreen() {
+  const navigate = useNavigate() ;
   return (
     <div className="RoodScreen">
       <h1>Rood Screen</h1>
+      <button className="backButton" onClick={() => navigate(-1)}>
+        Back to Tour
+      </button>
       <Footer />
     </div>
   );
--- src/components/WarMemorial.jsx
+++ src/components/WarMemorial.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Footer from './Footer'; 
+import { useNavigate } from "react-router-dom";
+import "./moreInfo.css";
 
 
 function WarMemorial() {
+  const navigate = useNavigate() ;
   return (
     <div className="WarMemorial">
       <h1>War Memorial</h1>
+      <button className="backButton" onClick={() => navigate(-1)}>
+        Back to Tour
+      </button>
       <Footer />
     </div>
   );
