b9436f66467276f98e9fcfd2c8c7526cfb29edfd
DuaneGAD <91886646+DuaneGAD@users.noreply.github.com>
2025-03-19 10:29:44 +0000

    Prompt Info
    Changed ui for prompt info
    fixed/disabled scroll issue on panorama

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import Layout from './components/Layout';
 import Info from './components/Info';
 import About from './components/About';
 import Organ from './components/Organ';
-import SmallPulpit from './components/SmallPulpit' ;
-import WarMemorial from './components/WarMemorial' ;
-import RoodScreen from './components/RoodScreen' ;
+import SmallPulpit from './components/SmallPulpit';
+import WarMemorial from './components/WarMemorial';
+import RoodScreen from './components/RoodScreen';
 import Panorama from './components/Panorama';
-
-
 
 function App() {
   return (
@@ -17,60 +15,49 @@ function App() {
         <Routes>
           <Route path="/" element={
             <Layout>
-
               <Info />
-
             </Layout>
-           }
-          />
+          }/>
           <Route path="/about" element={
             <Layout>
               <About />
             </Layout>
-           }
-          />
+          }/>
           <Route path="/panorama" element={
             <Layout>
-              <Panorama />
+              {/* Wrap the Panorama component */}
+              <div className="panorama-page">
+                <Panorama />
+                {/* Overlay Layer */}
+                <div className="panorama-overlay">
+                  {/* Add any overlay content here if needed */}
+                </div>
+              </div>
             </Layout>
-           }
-          />
+          }/>
           <Route path="/organ" element={
             <Layout>
-
               <Organ />
-
             </Layout>
-           }
-          />
+          }/>
           <Route path="/smallPulpit" element={
             <Layout>
-
               <SmallPulpit />
-
             </Layout>
-           }
-          />
+          }/>
           <Route path="/warMemorial" element={
             <Layout>
-
               <WarMemorial />
-
             </Layout>
-           }
-          />
+          }/>
           <Route path="/roodScreen" element={
             <Layout>
-
               <RoodScreen />
-
             </Layout>
-           }
-          />
+          }/>
         </Routes>
       </Router>
   );
 }
 
 export default App;
-
