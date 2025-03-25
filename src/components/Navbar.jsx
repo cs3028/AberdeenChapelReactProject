c7d97d9de8e3370a7c37213d1522cd595a70d643
ryansimm <145355767+ryansimm@users.noreply.github.com>
2025-03-25 19:47:28 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ useEffect(() => {
 
             <ul className={`navbar-links ${isOpen ? "active" : ""}`}>
 
-                <li><Link to="/anteChapel" onClick={closeMenu}>Ante-Chapel</Link></li>
+                <li><Link to="/panorama" onClick={closeMenu}>Ante-Chapel</Link></li>
                 <li><a href="/choirStallsPanorama" onClick={closeMenu}>Choir Stalls</a></li>
                 <li><a href="/sanctuaryPanorama" onClick={closeMenu}>Sanctuary</a></li>
                 <li><a href="/login" onClick={closeMenu}>Login</a></li>
